40eee7bde040ed5077c21d51b48d7d65a2570ec8
deminok <dominik.bluhm@gmail.com>
2019-07-06 12:04:00 +0200

    final commit

--- eval.py
+++ eval.py
@@ -4,6 +4,16 @@ plt.style.use('ggplot')
 
 
 def plot_reward(mean_scores, episodes, session_id):
+    """Evaluation function to visualize training progress
+    plots episodes against mean_scores and updates the graph in ./eval/
+    until the training session is over
+
+    :param
+        mean_scores: array of mean games scores averaged over the last training trainig episodes
+        episodes: array of numbers from zero to the current training episode
+        session_id: unique identifier for each training session, used to save graph for each
+        training session under a different name
+    """
     plt.figure(2)
     plt.clf()
     plt.title('Average Reward on Breakout')
@@ -11,4 +21,3 @@ def plot_reward(mean_scores, episodes, session_id):
     plt.ylabel('Reward')
     plt.plot(episodes, mean_scores)
     plt.savefig("./eval/results " + session_id + ".png")
-    #plt.show()
