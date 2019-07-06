8141534fe9acf9abfa81b6cd09d169083da1dd08
deminok <dominik.bluhm@gmail.com>
2019-07-06 12:13:24 +0200

    final commit

--- agent.py
+++ agent.py
@@ -10,9 +10,9 @@ class Agent():
     def __init__(self, device):
         self.epsilon = EPS_START
 
+        #set up policy and target DQN, optimizer and replay memory
         self.policy_net = DQN().to(device)
         self.target_net = DQN().to(device)
-
         self.optimizer = optim.RMSprop(self.policy_net.parameters(), lr=LEARNING_RATE,
                                        momentum=GRAD_MOM, eps=MIN_SQ_GRAD)
         self.memory = ReplayMemory()
@@ -20,25 +20,35 @@ class Agent():
         # initialize target net
         self.update_target_net()
 
-        #load previous model
-        if LOAD:
-            self.policy_net = torch.load('saved_model/breakout_dqn')
-
-    def select_action(self, frame, states): #0 NOOP, 1 fire, 2 right, 3 left
-        if (frame < RPL_START_SIZE) | (np.random.rand() <= self.epsilon):
+    def select_action(self, frame_nr, states): #0 NOOP, 1 fire, 2 right, 3 left
+        """
+        Selects an action based on epsilon greedy strategy
+        :param
+            frame_nr: current training frame, used to make sure that only random actions are selected
+            in the beginning
+            states: pytorch Tensor of size (1, 5, 84, 84), type torch.uint8 of which the first four
+            frames are forwarded through the policy net to calculate Q values for each possible action
+        :return: max Q value, integer indicating which action the agent should take: 0 NOOP, 1 fire, 2 right, 3 left
+        """
+        if (frame_nr < RPL_START_SIZE) | (np.random.rand() <= self.epsilon):
             action = random.randrange(ACTION_SIZE)
-            #print('frame: ', frame, ' random-action: ', action, ' | policy-action: ', int(self.policy_net(state).max(1)[1]))
+
         else:
             state = torch.tensor(np.float32(states[:, :4, :, :] / 255), dtype=torch.float32)
             action = self.policy_net(state)
             action = int(action.max(1)[1])
-            #print('frame: ', frame, ' action: ', action)
         return action
 
     def update_target_net(self):
+        """Copies weights of policy net to target net"""
         self.target_net.load_state_dict(self.policy_net.state_dict())
 
     def train(self):
+        """
+        Function to update parameters of the policy net
+        Samples minibatch and performs update iteration according to the algorithm
+        given by Mnih. et al. (2015)
+        """
         mini_batch = self.memory.sample()
         states, actions, rewards = [], [], []
         done = 0
@@ -75,13 +85,5 @@ class Agent():
         loss = F.mse_loss(input, target)
         loss.backward()
 
-
         #perform one update on policy_net
         self.optimizer.step()
-
-
-
-
-
-
-
